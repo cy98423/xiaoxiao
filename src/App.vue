af2f28535b126ba0a2526e2e0c512c11ae052495
Cy-Qy <308103222@qq.com>
2021-02-27 09:18:44 +0800

    修改导航栏样式

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,12 @@
   margin: 0;padding: 0;
   box-sizing: border-box;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  line-height: 1.5;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a{
+  text-decoration: none;
+  color: inherit;
 }
 
 
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <svg>
-        <use xlink:href="#label"/>
-      </svg>
-      记账
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon name="#money"/>记账
     </router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+    <router-link to="/labels" class="item">
+      <Icon name="#label"/>标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="#statistics"/>统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 
-  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-  try {
-    importAll(require.context('../assets/icons',true,/\.svg$/));
-  }catch (error){
-    console.log(error)
-  }
+
 
 
 export default {
@@ -29,7 +23,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
+  nav{
+    display: flex;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    flex-direction: row;
+    font-size: 12px;
+    > .item{
+      width: 33.333333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        width: 32px;
+        height: 32px;
+      }
+
+    }
   }
 </style>
