aad9e033d3030a7d98d4ea841468b6d6d5c0e65c
Cy-Qy <308103222@qq.com>
2021-02-28 18:14:43 +0800

    模块化

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'tags'
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow:1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background-color: #d9d9d9;
+      height: 24px;
+      line-height: 24px;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+      border-radius: (24px/2);
+
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
