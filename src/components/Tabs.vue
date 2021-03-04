5d1e4be8bb9572d9170899890260fde1d9956310
Cy-Qy <308103222@qq.com>
2021-03-04 16:36:24 +0800

    更新css

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,9 +1,10 @@
 <template>
-  <ul class="tabs">
+  <ul class="tabs" :class="{[classPrefix+'-tabs']: classPrefix}">
     <li
         v-for="item in dataSource"
         :key="item.value"
         @click="select(item)"
+        class="tabs-item"
         :class="liClass(item)"
     >{{item.text}}</li>
   </ul>
@@ -40,7 +41,7 @@ export default class Tabs extends Vue{
   text-align: center;
   font-size: 24px;
 
-  > li {
+  &-item {
     width: 50%;
     height: 64px;
     display: flex;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -48,4 +48,7 @@ export default class Statistics extends Vue {
     }
   }
 }
+::v-deep .interval-tabs-item{
+  height: 48px;
+}
 </style>
