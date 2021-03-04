0491d0ad61652b317b5f41661eb568b52457403d
Cy-Qy <308103222@qq.com>
2021-03-04 09:52:35 +0800

    Money.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -5,7 +5,7 @@
     </div>
     <ul class="current">
       <li
-          v-for="tag in dataSource"
+          v-for="tag in tagList"
           :key="tag.name"
           @click="toggle(tag.name)"
           :class="{selected: selectedTags.indexOf(tag.name)>=0}"
@@ -18,12 +18,20 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-import store from '@/store/index2';
+import {Component} from 'vue-property-decorator';
 
-@Component
+
+@Component({
+  computed:{
+    tagList(){
+      //TODO
+      // return this.$store.fetchTags()
+      return []
+    }
+  }
+})
 export default class Tags extends Vue {
-  @Prop(Array) readonly dataSource: string[] | undefined;
+  // tagList = store.fetchTags();
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -38,15 +46,11 @@ export default class Tags extends Vue {
 
   create() {
     const name = window.prompt('请输入标签名');
-    if (name === '') {
-      window.alert('标签名不能为空');
-    } else if (this.dataSource) {
-     if(name){
-       store.createTag(name);
-     }
-      //this.$emit('update:dataSource', [...this.dataSource,{id:name,name:name}]);
+    if (!name) {
+      return window.alert('标签名不能为空');
     }
-
+    //TODO
+    // store.createTag(name);
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,7 +9,7 @@
              @update:value="onUpdateNotes"
       ></FormItem>
       </div>
-      <Tags :data-source.sync="tags" @update:value="onUpdateTags"></Tags>
+      <Tags/>
     </layout>
 
 </template>
@@ -21,23 +21,25 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import store from '@/store/index2.ts';
+import store from '@/store/index.ts';
 
 @Component({
   components: {Tags, FormItem , Types, NumberPad},
+  computed:{
+    recordList(){
+      return store.state.recordList
+    }
+  }
 })
 export default class Money extends Vue {
-  tags = store.tagList;
-  recordList = store.recordList;
   record: RecordItem = {
     tags: [],
     notes: '',
     type: '+',
     amount: 0,
   };
-
-  onUpdateTags(value: string[]) {
-    this.record.tags = [JSON.stringify({id:value,name:value})];
+  created(){
+    this.$store.commit('fetchRecords')
   }
 
   onUpdateNotes(value: string) {
@@ -45,7 +47,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    store.createRecord(this.record)
+    this.$store.commit('createRecord',this.record)
   }
 
 }
