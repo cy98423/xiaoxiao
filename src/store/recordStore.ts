98cf3199ba4e92d309b7408ddadb4303a597138d
Cy-Qy <308103222@qq.com>
2021-03-05 08:49:53 +0800

    完成列表的CSS

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -15,7 +15,7 @@ const recordStore = {
   },
   createRecord(record: RecordItem) {
     const record2: RecordItem = clone(record);
-    record2.createdAt = new Date();
+    record2.createdAt = new Date().toISOString();
     this.recordList?.push(record2); //新语法,等价于this.recordList && this.recordList.push(record2)
     recordStore.saveRecords();
   },
